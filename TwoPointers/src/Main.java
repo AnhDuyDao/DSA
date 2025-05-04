fd7bea100422d04ea886408710e4e3ad51628240
Duy Anh <duyanhdao297@gmail.com>
2025-05-04 23:39:51 +0700

    Type one and 167.Two Sum II

--- TwoPointers/src/Main.java
+++ TwoPointers/src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
+    // Dang 1: Tim cap so co tong bang target
+    public static void pairSum(int arr[], int target) {
+        int left = 0; int right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                System.out.println(arr[left] + " + " + arr[right] + " = " + target);
+                break;
+            }
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    //167. Two Sum II - Input Array Is Sorted
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[] {left + 1, right + 1};
+            }
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[] {-1, -1}; // Not found
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        int arr[] = {1, 2, 4, 7, 11, 15};
+//        int target = 15;
+//        pairSum(arr, target);
+        int numbers[] = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(numbers, target);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
     }
 }
