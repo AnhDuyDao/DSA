45f2a0b3e0dd326bf29dd63b622bc67ca773e4aa
Duy Anh <duyanhdao297@gmail.com>
2025-05-04 23:16:32 +0700

    Sum and Count array

--- ArrayBasic/src/Main.java
+++ ArrayBasic/src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] a = {5, 2, 9, 1, 7};
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        System.out.println("Tổng = " + sum);
+
+        int max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        System.out.println("Max = " + max);
+
+        int X = 2;
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == X) {
+                count++;
+            }
+        }
+        System.out.println("Số lần xuất hiện của " + X + " là " + count);
     }
 }
